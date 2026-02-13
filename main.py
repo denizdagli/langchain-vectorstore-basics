c937bd18d6839459434a0aadd4fafd0c3b86a22a
denizdagli <denizdaglice@gmail.com>
2026-02-13 16:18:48 +0300

    feat: implement RAG chain using LangChain LCEL and OpenAI

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from dotenv import load_dotenv
 from langchain_core.documents import Document
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
-
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 load_dotenv()
 
 
@@ -35,7 +37,20 @@ vector_store = Chroma.from_documents(
     OpenAIEmbeddings(),
 )
 
+retriever = RunnableLambda(vector_store.similarity_search).bind(k=1)
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+message = """
+Answer this question using the provided context only.
+
+{question}
+Context: {context}
+
+"""
+
+prompt = ChatPromptTemplate.from_messages([("human", message)])
+chain ={"context":retriever,"question":RunnablePassthrough()} | prompt| llm
 if __name__ == "__main__":
-    embeddings = OpenAIEmbeddings().embed_query("dog")
-    print(vector_store.similarity_search_by_vector(embeddings,1))
+    response = chain.invoke("tell me about cats")
+    print(response.content)
     
